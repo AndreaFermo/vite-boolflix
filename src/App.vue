a8f989097924e1b0f3bb0c64a970a4c231c76012
Andrea Fermo <andreafermo.sd@gmail.com>
2023-03-31 15:29:28 +0200

    chiamata API con successo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<template></template>
+
+<script>
+import axios from 'axios';
+import { store } from './store.js';
+
+export default {
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    getMovies() {
+      let urlApi = 'https://api.themoviedb.org/3/search/movie?api_key=0ef6507b5d63cfd376dbac3c0f9236f2&query=ritorno+al+futuro&language=it';
+      axios.get(urlApi).then(response => {
+        this.store.moviesList = response.data;
+      }
+      )
+    }
+  },
+  created() {
+    this.getMovies();
+  }
+
+}
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+<style lang="scss"></style>
